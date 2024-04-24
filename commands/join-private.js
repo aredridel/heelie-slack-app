ad06835a48c9526b7930b6895ea06cef0d5b4286
Fiona Tay <me@fionatay.com>
2024-04-25 03:26:14 +0800

    /join-private: return if channel not found

--- commands/join-private.js
+++ commands/join-private.js
@@ -12,17 +12,20 @@ const joinPrivate = async ({
     return;
   }
 
+  // Slack Conversation APIs require the channel ID
+  // Look it up by fetching all private channels :(
   const chans = await Group.get(client);
   const channelName = command.text.replace(/^#/, '');
   const channel = chans.find((g) => g.name === channelName);
   if (!channel) {
-    respond(`#${channelName} not found. Heelie must be a member of #${channelName}.`);
+    await respond(`#${channelName} not found. Heelie must be a member of #${channelName}.`);
+    return;
   }
 
   await sendInviteRequest(client, channelName, command.user_id);
 
   const cocTxt = process.env.COC_URL ? `<${process.env.COC_URL}|Code of Conduct>` : 'Code of Conduct';
-  respond(`Join request sent. Please wait while the request is processed.\n\nRemember that private channels are not for allies unless otherwise specified and that there is a strong expectation of privacy in these channels -- what is said in there stays there.\n\nThe ${cocTxt} still fully applies in these spaces, with some channel-specific caveats (discussion of sexuality in lgbtq channels, for example).`);
+  await respond(`Invite request sent. Please wait while the request is processed.\n\nRemember that private channels are not for allies unless otherwise specified and that there is a strong expectation of privacy in these channels -- what is said in there stays there.\n\nThe ${cocTxt} still fully applies in these spaces, with some channel-specific caveats (discussion of sexuality in lgbtq channels, for example).`);
 };
 
 module.exports = { joinPrivate };
